41d8a9cc79ae92080308d8e4a1e15795fca4ddb2
nerijus-st <neneriukas@gmail.com>
2018-05-27 20:37:42 +0300

    use PROPAGATE_EXCEPTIONS in app

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from security import authenticate, identity
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'nerijus'
 api = Api(app)
 
