22c096b8ac9885cd68a17bb38897ea00841f62bf
nerijus-st <neneriukas@gmail.com>
2018-06-10 21:45:56 +0300

    implement refresh jwt token

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 
 from resources.item import Item, Items
 from resources.store import Store, Stores
-from resources.user import UserRegister, User, UserLogin
+from resources.user import UserRegister, User, UserLogin, TokenRefresh
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -17,11 +17,13 @@ api = Api(app)
 
 jwt = JWTManager(app)
 
+
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:
         return {'is_admin': True}
     return {'is_admin': False}
+
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
@@ -30,6 +32,7 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(UserLogin, '/login')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Stores, '/stores')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     from db import db
--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 
@@ -62,3 +62,11 @@ class UserLogin(Resource):
                        'refresh_token': refresh_token
                    }, 200
         return {'message': 'Invalid credentials'}, 401
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access token': new_token}, 200
