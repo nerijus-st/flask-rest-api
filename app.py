0c4321bd7ed91128c98d4c5daec3bf4a481415e6
nerijus-st <neneriukas@gmail.com>
2018-06-10 21:00:08 +0300

    Use JWT extended

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 
 from flask import Flask
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from resources.item import Item, Items
 from resources.store import Store, Stores
-from resources.user import UserRegister, User
-from security import authenticate, identity
+from resources.user import UserRegister, User, UserLogin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -16,12 +15,13 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'nerijus'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Stores, '/stores')
 
--- resources/item.py
+++ resources/item.py
@@ -1,4 +1,4 @@
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 from flask_restful import reqparse, Resource
 
 from models.item import ItemModel
@@ -16,7 +16,7 @@ class Item(Resource):
                         required=True,
                         help="Every item needs store_id")
 
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
